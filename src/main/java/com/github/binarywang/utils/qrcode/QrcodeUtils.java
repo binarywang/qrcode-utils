adbf37fc34f81a7515f179efe18acde6f294d23a
Oketa <zhuiqiuxinf@163.com>
2022-06-15 23:03:45 +0800

    modify QrcodeUtils

--- src/main/java/com/github/binarywang/utils/qrcode/QrcodeUtils.java
+++ src/main/java/com/github/binarywang/utils/qrcode/QrcodeUtils.java
@@ -1,25 +1,35 @@
 package com.github.binarywang.utils.qrcode;
 
-import com.google.common.base.Charsets;
-import com.google.common.collect.Maps;
-import com.google.zxing.*;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.awt.BasicStroke;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.nio.channels.FileChannel.MapMode;
-import java.nio.file.Files;
-import java.util.Map;
+import com.google.common.base.Charsets;
+import com.google.common.collect.Maps;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Binarizer;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * <pre>
@@ -39,20 +49,22 @@ public class QrcodeUtils {
    * @param content 二维码文字内容[为了信息安全性，一般都要先进行数据加密]
    * @param length  二维码图片宽度和高度
    */
-  private static BitMatrix createQrcodeMatrix(String content, int length) {
+  public static BitMatrix createQrcodeMatrix(String content, int length) throws Exception {
+    return createQrcodeMatrix(content, length, ErrorCorrectionLevel.H);
+  }
+  
+  public static BitMatrix createQrcodeMatrix(String content, int length, ErrorCorrectionLevel level) throws Exception {
     Map<EncodeHintType, Object> hints = Maps.newEnumMap(EncodeHintType.class);
     // 设置字符编码
     hints.put(EncodeHintType.CHARACTER_SET, Charsets.UTF_8.name());
     // 指定纠错等级
-    hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-
+    hints.put(EncodeHintType.ERROR_CORRECTION, level);
+    
     try {
       return new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, length, length, hints);
-    } catch (Exception e) {
-      logger.warn("内容为：【" + content + "】的二维码生成失败！", e);
-      return null;
+    } catch (WriterException e) {
+      throw new RuntimeException("内容为：【" + content + "】的二维码生成失败！", e);
     }
-
   }
 
   /**
@@ -64,31 +76,19 @@ public class QrcodeUtils {
    * @param logoConfig logo配置，可设置logo展示长宽，边框颜色
    * @return 二维码图片的字节数组
    */
-  public static byte[] createQrcode(String content, int length, File logoFile, MatrixToLogoImageConfig logoConfig) {
+  public static byte[] createQrcode(String content, int length, File logoFile, MatrixToLogoImageConfig logoConfig) throws Exception {
     if (logoFile != null && !logoFile.exists()) {
       throw new IllegalArgumentException("请提供正确的logo文件！");
     }
-
-    BitMatrix qrCodeMatrix = createQrcodeMatrix(content, length);
-    if (qrCodeMatrix == null) {
-      return null;
-    }
-    try {
-      File file = Files.createTempFile("qrcode_", "." + FORMAT).toFile();
-      logger.debug(file.getAbsolutePath());
-
-      MatrixToImageWriter.writeToFile(qrCodeMatrix, FORMAT, file);
-      if (logoFile != null) {
-        // 添加logo图片, 此处一定需要重新进行读取，而不能直接使用二维码的BufferedImage 对象
-        BufferedImage img = ImageIO.read(file);
-        overlapImage(img, FORMAT, file.getAbsolutePath(), logoFile, logoConfig);
-      }
-
-      return toByteArray(file);
-    } catch (Exception e) {
-      logger.warn("内容为：【" + content + "】的二维码生成失败！", e);
-      return null;
-    }
+    InputStream logo = new FileInputStream(logoFile);
+    return createQrcode(content, length, logo, logoConfig);
+  }
+  
+  public static byte[] createQrcode(String content, int length, InputStream logo, MatrixToLogoImageConfig logoConfig) throws Exception {
+    BufferedImage img =  generateQRCodeImage(content, length, logo, logoConfig);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ImageIO.write(img, FORMAT, baos);
+    return baos.toByteArray();
   }
 
   /**
@@ -99,7 +99,7 @@ public class QrcodeUtils {
    * @param logoFile logo 文件对象，可以为空
    * @return 二维码图片的字节数组
    */
-  public static byte[] createQrcode(String content, int length, File logoFile) {
+  public static byte[] createQrcode(String content, int length, File logoFile) throws Exception {
     return  createQrcode(content, length, logoFile, new MatrixToLogoImageConfig());
   }
 
@@ -111,29 +111,46 @@ public class QrcodeUtils {
    * @param logoFile logo 文件对象，可以为空
    * @return 二维码图片的字节数组
    */
-  public static byte[] createQrcode(String content, File logoFile) {
+  public static byte[] createQrcode(String content, File logoFile) throws Exception {
     return createQrcode(content, DEFAULT_LENGTH, logoFile);
   }
-
-  /**
-   * 将文件转换为字节数组，
-   * 使用MappedByteBuffer，可以在处理大文件时，提升性能
-   *
-   * @param file 文件
-   * @return 二维码图片的字节数组
-   */
-  private static byte[] toByteArray(File file) {
-    try (FileChannel fc = new RandomAccessFile(file, "r").getChannel();) {
-      MappedByteBuffer byteBuffer = fc.map(MapMode.READ_ONLY, 0, fc.size()).load();
-      byte[] result = new byte[(int) fc.size()];
-      if (byteBuffer.remaining() > 0) {
-        byteBuffer.get(result, 0, byteBuffer.remaining());
+  
+  public static BufferedImage generateQRCodeImage(String content, int length, InputStream logo, MatrixToLogoImageConfig logoConfig) throws Exception {
+    // 生成二维码图像
+    BitMatrix qrCodeMatrix = createQrcodeMatrix(content, length);
+    BufferedImage img =  MatrixToImageWriter.toBufferedImage(qrCodeMatrix);
+    try {
+      if (logo != null) {
+        overlapImage(img, FORMAT, logo, logoConfig);
       }
-      return result;
     } catch (Exception e) {
-      logger.warn("文件转换成byte[]发生异常！", e);
-      return null;
+      throw new RuntimeException("为二维码添加LOGO时失败！", e);
     }
+    return img;
+  }
+  
+  public static BufferedImage generateQRCodeImage(String content, int length, File logoFile, MatrixToLogoImageConfig logoConfig) throws Exception {
+    if (logoFile != null && !logoFile.exists()) {
+      throw new IllegalArgumentException("请提供正确的logo文件！");
+    }
+    InputStream logo = new FileInputStream(logoFile);
+    return generateQRCodeImage(content, length, logo, logoConfig);
+  }
+  
+  public static BufferedImage generateQRCodeImage(String content, int length, InputStream logo) throws Exception {
+    return generateQRCodeImage(content, length, logo, new MatrixToLogoImageConfig());
+  }
+  
+  public static BufferedImage generateQRCodeImage(String content, int length, File logoFile) throws Exception {
+    return generateQRCodeImage(content, length, logoFile, new MatrixToLogoImageConfig());
+  }
+  
+  public static BufferedImage generateQRCodeImage(String content, InputStream logo) throws Exception {
+    return generateQRCodeImage(content, DEFAULT_LENGTH, logo);
+  }
+  
+  public static BufferedImage generateQRCodeImage(String content, File logoFile) throws Exception {
+    return generateQRCodeImage(content, DEFAULT_LENGTH, logoFile);
   }
 
   /**
@@ -144,32 +161,26 @@ public class QrcodeUtils {
    * @param logoFile  logo文件对象
    * @param format    图片格式
    */
-  private static void overlapImage(BufferedImage image, String format, String imagePath, File logoFile,
-                                   MatrixToLogoImageConfig logoConfig) throws IOException {
-    try {
-      BufferedImage logo = ImageIO.read(logoFile);
-      Graphics2D g = image.createGraphics();
-      // 考虑到logo图片贴到二维码中，建议大小不要超过二维码的1/5;
-      int width = image.getWidth() / logoConfig.getLogoPart();
-      int height = image.getHeight() / logoConfig.getLogoPart();
-      // logo起始位置，此目的是为logo居中显示
-      int x = (image.getWidth() - width) / 2;
-      int y = (image.getHeight() - height) / 2;
-      // 绘制图
-      g.drawImage(logo, x, y, width, height, null);
+  private static void overlapImage(final BufferedImage image, String format, final InputStream logo,
+      MatrixToLogoImageConfig logoConfig) throws IOException {
+    BufferedImage logoImg = ImageIO.read(logo);
+    Graphics2D g = image.createGraphics();
+    // 考虑到logo图片贴到二维码中，建议大小不要超过二维码的1/5;
+    int width = image.getWidth() / logoConfig.getLogoPart();
+    int height = image.getHeight() / logoConfig.getLogoPart();
+    // logo起始位置，此目的是为logo居中显示
+    int x = (image.getWidth() - width) / 2;
+    int y = (image.getHeight() - height) / 2;
+    // 绘制图
+    g.drawImage(logoImg, x, y, width, height, null);
 
-      // 给logo画边框
-      // 构造一个具有指定线条宽度以及 cap 和 join 风格的默认值的实心 BasicStroke
-      g.setStroke(new BasicStroke(logoConfig.getBorder()));
-      g.setColor(logoConfig.getBorderColor());
-      g.drawRect(x, y, width, height);
+    // 给logo画边框
+    // 构造一个具有指定线条宽度以及 cap 和 join 风格的默认值的实心 BasicStroke
+    g.setStroke(new BasicStroke(logoConfig.getBorder()));
+    g.setColor(logoConfig.getBorderColor());
+    g.drawRect(x, y, width, height);
 
-      g.dispose();
-      // 写入logo图片到二维码
-      ImageIO.write(image, format, new File(imagePath));
-    } catch (Exception e) {
-      throw new IOException("二维码添加logo时发生异常！", e);
-    }
+    g.dispose();
   }
 
   /**
