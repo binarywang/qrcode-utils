9b7ace9164deb2895f436f142ec9e25d2e863d5f
Binary Wang <binarywang@gmail.com>
2022-06-21 21:02:59 +0800

    :art: fix test

--- src/test/java/com/github/binarywang/utils/qrcode/QrcodeUtilsTest.java
+++ src/test/java/com/github/binarywang/utils/qrcode/QrcodeUtilsTest.java
@@ -1,6 +1,15 @@
 package com.github.binarywang.utils.qrcode;
 
-import java.awt.Desktop;
+import com.beust.jcommander.internal.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.BasicConfigurator;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
@@ -8,79 +17,67 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import javax.imageio.ImageIO;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.log4j.BasicConfigurator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import com.beust.jcommander.internal.Lists;
-
 /**
  * <pre>
  * Created by Binary Wang on 2017-01-05.
  * @author <a href="https://github.com/binarywang">binarywang(Binary Wang)</a>
  * </pre>
  */
+@Slf4j
 public class QrcodeUtilsTest {
-    private static Logger logger = LoggerFactory.getLogger(QrcodeUtils.class);
 
-    private String content = "abc";
-    private List<Path> generatedQrcodePaths = Lists.newArrayList();
+  private String content = "abc";
+  private List<Path> generatedQrcodePaths = Lists.newArrayList();
 
-    @BeforeTest
-    public void setup() {
-        BasicConfigurator.configure();
+  @BeforeTest
+  public void setup() {
+    BasicConfigurator.configure();
+  }
+
+  @Test
+  public void testCreateQrcode() throws Exception {
+    byte[] bytes = QrcodeUtils.createQrcode(content, 800, null);
+    Path path = Files.createTempFile("qrcode_800_", ".jpg");
+    generatedQrcodePaths.add(path);
+    log.info("{}", path.toAbsolutePath());
+    Files.write(path, bytes);
+
+    bytes = QrcodeUtils.createQrcode(content, null);
+    path = Files.createTempFile("qrcode_400_", ".jpg");
+    generatedQrcodePaths.add(path);
+    log.info("{}", path.toAbsolutePath());
+    Files.write(path, bytes);
+  }
+
+  @Test
+  public void testCreateQrcodeWithLogo() throws Exception {
+    try (InputStream inputStream = ClassLoader.getSystemResourceAsStream("logo.png")) {
+      File logoFile = Files.createTempFile("logo_", ".jpg").toFile();
+      FileUtils.copyInputStreamToFile(inputStream, logoFile);
+      log.info("{}", logoFile);
+      byte[] bytes = QrcodeUtils.createQrcode(content, 800, logoFile);
+      Path path = Files.createTempFile("qrcode_with_logo_", ".jpg");
+      generatedQrcodePaths.add(path);
+      log.info("{}", path.toAbsolutePath());
+      Files.write(path, bytes);
     }
+  }
 
-    @Test
-    public void testCreateQrcode() throws Exception {
-        byte[] bytes = QrcodeUtils.createQrcode(content, 800, null);
-        Path path = Files.createTempFile("qrcode_800_", ".jpg");
-        generatedQrcodePaths.add(path);
-        logger.info("{}", path.toAbsolutePath());
-        Files.write(path, bytes);
-
-        bytes = QrcodeUtils.createQrcode(content, null);
-        path = Files.createTempFile("qrcode_400_", ".jpg");
-        generatedQrcodePaths.add(path);
-        logger.info("{}", path.toAbsolutePath());
-        Files.write(path, bytes);
+  @Test(dependsOnMethods = {"testCreateQrcode", "testCreateQrcodeWithLogo"})
+  public void testDecodeQrcode() throws Exception {
+    for (Path path : generatedQrcodePaths) {
+      Assert.assertEquals(QrcodeUtils.decodeQrcode(path.toFile()), content);
     }
-
-    @Test
-    public void testCreateQrcodeWithLogo() throws Exception {
-        try (InputStream inputStream = ClassLoader.getSystemResourceAsStream("logo.png")) {
-            File logoFile = Files.createTempFile("logo_", ".jpg").toFile();
-            FileUtils.copyInputStreamToFile(inputStream, logoFile);
-            logger.info("{}",logoFile);
-            byte[] bytes = QrcodeUtils.createQrcode(content, 800, logoFile);
-            Path path = Files.createTempFile("qrcode_with_logo_", ".jpg");
-            generatedQrcodePaths.add(path);
-            logger.info("{}",path.toAbsolutePath());
-            Files.write(path, bytes);
-        }
-    }
-
-    @Test(dependsOnMethods = {"testCreateQrcode", "testCreateQrcodeWithLogo"})
-    public void testDecodeQrcode() throws Exception {
-        for (Path path : generatedQrcodePaths) {
-            Assert.assertEquals(QrcodeUtils.decodeQrcode(path.toFile()), content);
-        }
-    }
+  }
 
 
-    @Test
-    public void testPersonCode() throws Exception {
-        BufferedImage img = QrcodeUtils.generateQRCodeImage(content, 400, ClassLoader.getSystemResourceAsStream("logo.png"));
-        File outFile = Files.createTempFile("qrcode_with_logo_", ".png").toFile();
-        logger.info("{}", outFile.getAbsolutePath());
-        ImageIO.write(img, "png", outFile);
-        Desktop.getDesktop().open(outFile);
-    }
+  @Test
+  public void testPersonCode() throws Exception {
+    BufferedImage img = QrcodeUtils.generateQRCodeImage(content, 400, ClassLoader.getSystemResourceAsStream("logo.png"));
+    File outFile = Files.createTempFile("qrcode_with_logo_", ".png").toFile();
+    log.info("{}", outFile.getAbsolutePath());
+    ImageIO.write(img, "png", outFile);
+    Desktop.getDesktop().open(outFile);
+  }
 
 }
